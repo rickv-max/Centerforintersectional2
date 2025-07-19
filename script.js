caf5ef13ff7b3d029fd46aab9d74f529425596d2
rickv-max <rickymaulana080503@gmail.com>
2025-07-20 00:35:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-// Toggle Menu
 const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
+const navbar = document.getElementById("navbar");
 
 hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
-
-// Modal
-const openModal = document.getElementById("openModal");
-const closeModal = document.getElementById("closeModal");
-const modal = document.getElementById("contact-modal");
-
-openModal.addEventListener("click", () => {
-  modal.style.display = "flex";
-});
-
-closeModal.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-window.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
+  navbar.classList.toggle("active");
 });
